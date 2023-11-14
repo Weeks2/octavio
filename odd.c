c2dfa20f16cbd9f3db91deec5838bb8faa5822f6
Christian Ibarra <chris.cjarne@gmail.com>
2023-11-13 19:28:57 -0600

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-int main() {
-    for(int number = 1; number <= 10; number++) {
+
+void calculate_odd(int qty) {
+    for(int number = 1; number <= qty; number++) {
         if(number % 2 != 0) {
             printf("%d\n", number);
         }
     }
+}
+
+int main() {
+    // get qty
+    // if qty < 10 : msg -- calculate_odd(qty)
+    calculate_odd(1000000000000000);
     return 0;
 }
