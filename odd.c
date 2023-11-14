1ce83b1d2b2ee5902cc910437b988c2fedf77d6c
colp29 <150750783+colp29@users.noreply.github.com>
2023-11-13 19:03:20 -0600

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int quantity = 10;
+    int number = 1;
+    int i = 0;
+    for(i = 0; i < quantity; i++) {
+        printf("%d", &number);
+        number += 2;
+        }
+    return 0;
+    }
