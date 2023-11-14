abfadcd64b811a7ea7703aa2446f4df31af4a3b7
Christian Ibarra <chris.cjarne@gmail.com>
2023-11-13 19:07:14 -0600

    Update odd.c

--- odd.c
+++ odd.c
@@ -4,7 +4,7 @@ int main() {
     int number = 1;
     int i = 0;
     for(i = 0; i < quantity; i++) {
-        printf("%d", &number);
+        printf("%d\n", &number);
         number += 2;
         }
     return 0;
